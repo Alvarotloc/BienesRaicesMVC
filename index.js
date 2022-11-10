4598783f30eee90a50f49a107203789b96f8e9ac
Alvarotloc <alvarotloc@gmail.com>
2022-11-10 10:04:07 +0100

    Añadido tailwind y creado principio de layout con pug

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-import express from 'express';
+import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
 
 // Crear la app
-const app = express();
+const app = express()
 
 // Habilitar PUG
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+// Carpeta Pública
+app.use(express.static('public'))
+
 // Routing
 app.use('/auth', usuarioRoutes)
 
 // Definimos un puerto y ponemos a la aplicación a escucharlo
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`)
+  console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
