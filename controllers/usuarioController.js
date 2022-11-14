3f4975fea4b8de4db6c7f78a7b883c411e487036
Alvarotloc <alvarotloc@gmail.com>
2022-11-14 09:04:58 +0100

    Pagina de registro casi creada, faltan enlaces internos para redirigir a login y a olvide password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,10 +1,12 @@
 const formularioLogin = (req, res) => {
   res.render('auth/login', {
+    title: 'Iniciar Sesión'
   })
 }
 
 const formularioRegistro = (req, res) => {
   res.render('auth/registro', {
+    title: 'Crea Tu Cuenta'
   })
 }
 
