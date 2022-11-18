7a67ebc73fcf071f2b75e40d4e3051c036cd126e
Alvarotloc <alvarotloc@gmail.com>
2022-11-18 12:57:10 +0100

    Añadida función para editar propiedades, siguiente paso eliminarlas

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -4,9 +4,22 @@ import Categoria from '../models/Categoria.js'
 import Precio from '../models/Precio.js'
 import { Propiedad } from '../models/index.js'
 
-const admin = (req, res) => {
+const admin = async (req, res) => {
+  const { id } = req.usuario
+
+  const propiedades = await Propiedad.findAll({
+    where: {
+      usuarioId: id
+    },
+    include: [
+      { model: Categoria, as: 'categoria' },
+      { model: Precio, as: 'precio' }
+    ]
+  })
+
   res.render('propiedades/admin', {
-    title: 'Mis Propiedades'
+    title: 'Mis Propiedades',
+    propiedades
   })
 }
 
@@ -131,10 +144,101 @@ const almacenarImagenes = async (req, res, next) => {
   }
 }
 
+const formularioEditar = async (req, res) => {
+  const { id } = req.params
+  const { id: idUsuario } = req.usuario
+
+  const [categorias, precios, propiedad] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll(),
+    Propiedad.findByPk(id)
+  ])
+
+  if (!propiedad) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  // La propiedad pertenece a quien visita la pag
+  if (idUsuario.toString() !== propiedad.usuarioId.toString()) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  res.render('propiedades/editarPropiedad', {
+    title: `Editar Propiedad: ${propiedad.titulo}`,
+    csrfToken: req.csrfToken(),
+    categorias,
+    precios,
+    datos: propiedad
+  })
+}
+
+const editarPropiedad = async (req, res) => {
+  // Validación
+  const { id } = req.params
+  const { id: idUsuario } = req.usuario
+
+  const resultado = validationResult(req)
+
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll()
+  ])
+
+  if (!resultado.isEmpty()) {
+    return res.render('propiedades/editarPropiedad', {
+      title: 'Editar Propiedad',
+      categorias,
+      precios,
+      csrfToken: req.csrfToken(),
+      errores: resultado.array(),
+      datos: req.body
+    })
+  }
+
+  const propiedad = await Propiedad.findByPk(id)
+
+  if (!propiedad) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  // La propiedad pertenece a quien visita la pag
+  if (idUsuario.toString() !== propiedad.usuarioId.toString()) {
+    return res.redirect('/mis-propiedades')
+  }
+
+  try {
+    const { titulo, descripcion, habitaciones, estacionamiento, banhos, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body
+    propiedad.set({
+      titulo,
+      descripcion,
+      habitaciones,
+      estacionamiento,
+      banhos,
+      calle,
+      lat,
+      lng,
+      precioId,
+      categoriaId
+    })
+    await propiedad.save(
+      res.redirect('/mis-propiedades')
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const eliminarPropiedad = async (req, res) => {
+
+}
+
 export {
   admin,
   crearPropiedad,
   guardarPropiedad,
   agregarImagen,
-  almacenarImagenes
+  almacenarImagenes,
+  formularioEditar,
+  editarPropiedad,
+  eliminarPropiedad
 }
